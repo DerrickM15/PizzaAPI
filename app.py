5e6ccb520576ad38d6e52141f4e1edb5f55ddcec
DerrickM15 <marzend88@gmail.com>
2023-09-29 11:03:08 -0500

    convert to pycharm. Reconfigured venv

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask, request
-import json, time
-from base import Session, Base, engine
-from users import User, UserRequest, UserResponseModel, UserUpdate
-from address import Address
-import sqlalchemy
+import json
 import sqlite3
+import time
+
 import argon2
+import sqlalchemy
+from flask import Flask, request, jsonify
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 from pydantic import ValidationError
 
+from address import Address
+from base import Session, Base, engine
+from users import User, UserRequest, UserResponseModel, UserUpdate
+
 app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = "this-secret-key"
+jwt = JWTManager(app)
+
 
 # hash passwords
 def hash_password(password): 
@@ -18,10 +28,11 @@ def hash_password(password):
     hash = argon2Hasher.hash(password).replace(hash_prefix, "")
     return hash
 
+
 @app.route('/register', methods=['POST'])
 def register(): 
     request_data = request.get_json()
-    user_request = UserRequest(**{ k.lower():v for (k,v) in request_data.items()})
+    user_request = UserRequest(**{k.lower(): v for (k, v) in request_data.items()})
 
     # Hash password
     user_request.password = hash_password(user_request.password)
@@ -40,9 +51,10 @@ def register():
     except ValidationError as e: 
         return e, 400
 
-    UserResponse = UserResponseModel.from_orm(user)
+    user_response = UserResponseModel.from_orm(user)
     session.close()
-    return UserResponse.dict(), 201
+    return user_response.dict(), 201
+
 
 @app.route('/update', methods=['POST'])
 def userupdate():
@@ -51,103 +63,112 @@ def userupdate():
     data = UserUpdate(**{ k.lower():v for (k,v) in request_data.items()})
     if data.id is None: 
         return "Must provide a User ID", 400
-    else: 
-        DatabaseUser = session.query(User).filter_by(id=data.id).first()
-        DatabaseAddress = session.query(Address).filter_by(id=data.id).first()
-        if data.name is not None: 
-            DatabaseUser.name= data.name
-        if data.username is not None: 
-            DatabaseUser.username= data.username
-        if data.email is not None: 
-            DatabaseUser.email= data.email
-        if data.password is not None: 
-            DatabaseUser.password= hash_password(data.password)
-        if data.address is not None: 
-            DatabaseAddress.address= data.address
-        if data.city is not None: 
-            DatabaseAddress.city= data.city
-        if data.state is not None: 
-            DatabaseAddress.state= data.state
-        if data.zipcode is not None: 
-            DatabaseAddress.zipcode= data.zipcode
-        session.add(DatabaseUser)
-        FoundUser = UserResponseModel.from_orm(DatabaseUser)
+    else:
+        database_user = session.query(User).filter_by(id=data.id).first()
+        database_address = session.query(Address).filter_by(id=data.id).first()
+        if data.name is not None:
+            database_user.name = data.name
+        if data.username is not None:
+            database_user.username = data.username
+        if data.email is not None:
+            database_user.email = data.email
+        if data.password is not None:
+            database_user.password = hash_password(data.password)
+        if data.address is not None:
+            database_address.address = data.address
+        if data.city is not None:
+            database_address.city = data.city
+        if data.state is not None:
+            database_address.state = data.state
+        if data.zipcode is not None:
+            database_address.zipcode = data.zipcode
+        session.add(database_user)
+        found_user = UserResponseModel.from_orm(database_user)
         session.commit()
         session.close()
-        return FoundUser.dict()
-    
+        return found_user.dict()
 
 
-# Login
-# @app.route('/login', methods=['POST'])
-# def login():
-#     request_data = request.get_json()
-#     user_username = request_data.get('Username')
-#     user_password = request_data.get('Password')
-#     # Establish connection to the DB and retrieve hashed password
-#     conn = sqlite3.connect('pizza_users.db')
-#     cur = conn.cursor()
-#     cur.execute('''SELECT password FROM users WHERE username=?''', (user_username,))
-#     # Put hash params back onto hashed password
-#     hash_prefix = "$argon2id$v=19$m=32,t=4,p=1$"
-#     hash = hash_prefix + "".join(cur.fetchone())
-#     # Verify password matches hashed password, return error if not
-#     argon2Hasher = argon2.PasswordHasher(
-#         time_cost=4, memory_cost=2**5, parallelism=1, hash_len=32, salt_len=16)
-#     try:
-#         argon2Hasher.verify(hash, user_password)
-#         data_set = {'Message:': f'{user_username} logged in successfully', 'Timestamp': time.time()}
-#         json_dump = json.dumps(data_set)
-#         # TODO: learn about the authentication token nonsense and put it here
-#         return json_dump
-#     except:
-#         data_set = {'Message:': "Incorrect Password", 'Timestamp': time.time()}
-#         json_dump = json.dumps(data_set)
-#         return json_dump, 400
+@app.route('/login', methods=['POST'])
+def login():
+    request_data = request.get_json()
+    user_username = request_data.get('Username')
+    user_password = request_data.get('Password')
+    # Establish connection to the DB and retrieve hashed password
+    conn = sqlite3.connect('pizza_users.db')
+    cur = conn.cursor()
+    try:
+        cur.execute('''SELECT password FROM users WHERE username=?''', (user_username,))
+    except:
+        data_set = {'Message:': "Username does not exist", 'Timestamp': time.time()}
+        json_dump = json.dumps(data_set)
+        return json_dump, 401
+    # Put hash params back onto hashed password
+    hash_prefix = "$argon2id$v=19$m=32,t=4,p=1$"
+    hash = hash_prefix + "".join(cur.fetchone())
+    # Verify password matches hashed password, return error if not
+    argon2Hasher = argon2.PasswordHasher(
+        time_cost=4, memory_cost=2 ** 5, parallelism=1, hash_len=32, salt_len=16)
+    try:
+        argon2Hasher.verify(hash, user_password)
+    except:
+        data_set = {'Message:': "Incorrect Password", 'Timestamp': time.time()}
+        json_dump = json.dumps(data_set)
+        return json_dump, 401
+    access_token = create_access_token(identity=user_username)
+    return jsonify(access_token=access_token)
 
-# TODO: Alter user data
 
-# TODO: Order 
-@app.route('/order', methods=['POST'])
-def order():
-    # TODO: Repeat order option
-    # TODO: Unsuccesful Order case
-    # TODO: Successful Order case, provide order and price
-    data_set = {'Message:': "Order Received", 'Timestamp': time.time()}
-    json_dump = json.dumps(data_set)
-    return json_dump, 400
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+
+# TODO: Order
+# @app.route('/order', methods=['POST'])
+# def order():
+#     # TODO: Repeat order option
+#     # TODO: Unsuccesful Order case
+#     # TODO: Successful Order case, provide order and price
+#     data_set = {'Message:': "Order Received", 'Timestamp': time.time()}
+#     json_dump = json.dumps(data_set)
+#     return json_dump, 400
+
 
 # TODO: Scrap all of this and replace it with something better
-@app.route('/menu', methods=['GET'])
-def menu():
-    ingredients = set([
-    "Hand Tossed Crust", 
-    "Classic Tomato Sauce", 
-    "Shredded Mozzarella Cheese", 
-    "Pepperoni", 
-    "Green Peppers", 
-    "Seasoned Pork Sausage", 
-    "Beef", 
-    "Onions", 
-    "Black Olives", 
-    "Mushrooms", 
-    "Italian Sausage", 
-    "Ham", 
-    "Pineapple", 
-    "Brown Sugar Hand Tossed Crust", 
-    "Cherry Filling", 
-    "Crumb Topping", 
-    "Icing Swirl"
-    ])
+# @app.route('/menu', methods=['GET'])
+# def menu():
+#     ingredients = set([
+#     "Hand Tossed Crust", 
+#     "Classic Tomato Sauce", 
+#     "Shredded Mozzarella Cheese", 
+#     "Pepperoni", 
+#     "Green Peppers", 
+#     "Seasoned Pork Sausage", 
+#     "Beef", 
+#     "Onions", 
+#     "Black Olives", 
+#     "Mushrooms", 
+#     "Italian Sausage", 
+#     "Ham", 
+#     "Pineapple", 
+#     "Brown Sugar Hand Tossed Crust", 
+#     "Cherry Filling", 
+#     "Crumb Topping", 
+#     "Icing Swirl"
+#     ])
 
-    pizzas = {
-    "Pepperoni": {"Ingredients": [ingredients[1:4]]}, 
-    "Cheese": {"Ingredients": [ingredients[1:3]]}, 
-    "Supreme": {"Ingredients": [ingredients[1:11]]},
-    "Meat Lover's": {"Ingredients": [ingredients[1:7], ingredients[11:12]]}, 
-    "Hawaiian": {"Ingredients": [ingredients[1:3], ingredients[12:13]]}, 
-    "Dessert": {"Ingredients": [ingredients[14:17]]}
-    }
+#     pizzas = {
+#     "Pepperoni": {"Ingredients": [ingredients[1:4]]}, 
+#     "Cheese": {"Ingredients": [ingredients[1:3]]}, 
+#     "Supreme": {"Ingredients": [ingredients[1:11]]},
+#     "Meat Lover's": {"Ingredients": [ingredients[1:7], ingredients[11:12]]}, 
+#     "Hawaiian": {"Ingredients": [ingredients[1:3], ingredients[12:13]]}, 
+#     "Dessert": {"Ingredients": [ingredients[14:17]]}
+#     }
 
 
 if __name__ == '__main__':
